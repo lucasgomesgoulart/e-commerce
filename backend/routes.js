bf456872f220beade19fee8a5a7532c26ab896a3
Lucas Goulart <lucasgoulartg@hotmail.com>
2023-02-23 18:01:22 -0300

    alteracoes do service

--- backend/routes.js
+++ backend/routes.js
@@ -3,7 +3,9 @@ const UserController = require('./api/controllers/UserController')
 
 const router = Router()
 
+router.post('/login', UserController.login)
 router.get('/login', UserController.login)
 router.post('/createUser', UserController.createUser)
+router.delete('/deleteUser/:id', UserController.deleteUser)
 
 module.exports = router
